671dc858ecd10e09c6cddda180da35f971bd9182
mohamed <mhalwasoffice@gmail.com>
2024-08-20 19:17:09 +0100

    updating

--- src/componants/header.jsx
+++ src/componants/header.jsx
@@ -1,17 +1,44 @@
 import "../App.css";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import myImg from "../assets/logo.svg";
 export default function Header() {
+  const [isMenuopen, setisMenuOpen] = useState(false);
   return (
     <div className="header flex justify-between items-center py-4">
-      <div className="menu-toggle">
-        <div className="cursor-pointer">
-          <span className="block w-10 h-1 rounded-xl mb-1 bg-black"></span>
-          <span className="block w-6 h-1 rounded-xl bg-black"></span>
-          <span className="block w-12 h-1 rounded-xl my-1 bg-black"></span>
+      <div
+        className="menu-toggle"
+        onClick={() => {
+          setisMenuOpen(!isMenuopen);
+        }}
+      >
+        <div className="cursor-pointer relative z-40">
+          <span
+            className="block w-10 h-1 rounded-xl mb-1 bg-black transition duration-main-transition ease-main-transition"
+            style={{
+              transitionProperty : 'all',
+              width: isMenuopen ? '0px' : '2.25rem',
+              opacity: isMenuopen ? "0" : "1",
+            }}
+            ></span>
+          <span className="block w-6 h-1 rounded-xl bg-black duration-main-transition ease-main-transition"
+          style={{
+            transitionProperty : 'all',
+            width:  isMenuopen ? '2.25rem ' : '1.5rem',
+          }}
+          ></span>
+          <span
+            className="block w-12 h-1 rounded-xl my-1 bg-black transition duration-main-transition ease-main-transition"
+            style={{
+              transitionProperty : 'all',
+              width: isMenuopen ? '0px' : '2.25rem',
+              opacity: isMenuopen ? "0" : "1",
+            }}
+          ></span>
         </div>
-        <div>
-          <ul>
+        <div className="fixed w-full h-full bg-gray-300 left-0 top-0 z-10">
+          <ul className="w-full h-full flex justify-center items-start pl-32 gap-20 flex-col text-5xl">
             <li>
               <NavLink to="./">Home</NavLink>
             </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#292929",
-      }
+      },
+      transitionDuration: {
+        'main-transition': '1s',
+      },
+      transitionTimingFunction: {
+        'main-transition': 'cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
-}
-
+};
